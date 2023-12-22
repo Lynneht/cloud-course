9e0e07414ed6b19f403bed61d7255b7cbf745e57
Lynneht <153532733+Lynneht@users.noreply.github.com>
2023-12-22 16:11:05 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
+from typing import Union
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Spain"}
+
+
+@app.get("pip install fastapi/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    retunr {"item_id":item_id, "q": q}
